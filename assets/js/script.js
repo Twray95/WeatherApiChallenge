5401cc6f7fd5e222303cff9bfc8c2bee38264666
Thomas Wray <twray95@gmail.com>
2023-02-22 16:50:05 -0700

    searches now add the user input inot local storage and create a button for each item in local storage to create past search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,50 @@
 var searchFieldEl = document.querySelector("#search-input");
 var searchBtnEl = document.querySelector("#searchBtn");
+var pastSearchEl = document.querySelector("#pastSearches");
 var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 var weatherApiKey = "&appid=bb7d0040d30c625c65c4836bd0556ffe";
 var today = dayjs();
 var weatherTodayEl = document.querySelector("#today");
+var pastSearchArray = [];
+
+function cityBtn() {
+  var cityList = JSON.parse(localStorage.getItem("pastSearch"));
+  console.log(cityList);
+  pastSearchEl.textContent = "";
+  for (i = 0; i < cityList.length; i++) {
+    var btnDiv = document.createElement("div");
+    btnDiv.classList.add("form-group");
+    pastSearchEl.appendChild(btnDiv);
+    var cityBtnEl = document.createElement("button");
+    cityBtnEl.classList.add("btn");
+    cityBtnEl.classList.add("btn-secondary");
+    cityBtnEl.classList.add("btn-block");
+    cityBtnEl.textContent = cityList[i];
+    btnDiv.appendChild(cityBtnEl);
+  }
+}
 
 $(searchBtnEl).on("click", function (event) {
   event.preventDefault();
-
-  var userSearch =
+  var userSearch = searchFieldEl.value;
+  pastSearchArray.push(userSearch);
+  localStorage.setItem("pastSearch", JSON.stringify(pastSearchArray));
+  var userSearchURL =
     weatherUrl +
     searchFieldEl.value +
     "&length=1" +
     weatherApiKey +
     "&units=imperial";
-  console.log(userSearch);
   if (!searchFieldEl.value) {
     console.log("please search a city");
   } else {
-    fetch(userSearch)
+    fetch(userSearchURL)
       .then(function (response) {
         return response.json();
       })
 
       .then(function (data) {
-        console.log(data);
-        console.log(data.name + data.main.temp);
+        weatherTodayEl.textContent = "";
         var weatherToday = document.createElement("h1");
         weatherToday.textContent = data.name + today.format(" (M / D / YYYY)");
         weatherTodayEl.appendChild(weatherToday);
@@ -38,4 +57,5 @@ $(searchBtnEl).on("click", function (event) {
         weatherToday.appendChild(tempToday);
       });
   }
+  cityBtn();
 });
